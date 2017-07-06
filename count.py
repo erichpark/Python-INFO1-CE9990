97e1b00759aa0a220725e74cba03ad354e7e887a
Eric Park <ehp349@gmail.com>
2017-07-06 15:15:30 -0400

    Update count.py

--- count.py
+++ count.py
@@ -5,96 +5,117 @@ Eric Park
 INFO1-CE9990 Section 2
 Homework for June 29, 2017
 
-Count to 10 in Two Languages
-
+Count to 10 in Japanese and Russian, ascending and descending
 """
 
 import sys
 
+#a is a list containing two items: a[0] is Japanese and a[1] is Russian.
+#a[0] is a list containing two items: a[0][0] is Japanese ascending,
+#and a[0][1] is Japanese descending.
+#a[1] is a list containing two items: a[1][0] is Russian ascending,
+#and a[1][1] is Russian descending.
+#Finally, a[0][0] is a list containing 10 items.
+
+a = [
+    [
+        [
+            "ichi  (いち)", #Japanese ascending, 1 to 10
+            "ni    (に)",
+            "san   (さん)",
+            "yon   (よん)",
+            "go    (ご)",
+            "roku  (ろく)",
+            "nana  (なな)",
+            "hachi (はち)",
+            "kyuu  (きゅう)",
+            "jyuu  (じゅう)"
+        ],
+        [
+            "jyuu  (じゅう)",  #Japanese descending, 10 to 1
+            "kyuu  (きゅう)",
+            "hachi (はち)",
+            "nana  (なな)",
+            "roku  (ろく)",
+            "go    (ご)",
+            "yon   (よん)",
+            "san   (さん)",
+            "ni    (に)",
+            "ichi  (いち)"
+        ],
+    ],
+
+    [
+        [
+            "a-deen      (Один)", #Russian ascending, 1 to 10
+            "dva         (Два)",
+            "tree        (Три)",
+            "chye-tir-ye (Четыре)",
+            "pyat        (Пять)",
+            "shest       (Шесть)",
+            "syem        (Семь)",
+            "vo-syem     (Восемь)",
+            "dyev-yat    (Девять)",
+            "dyes-yat    (Десять)"
+        ],
+        [
+            "dyes-yat    (Десять)", #Russian descending 10 to 1
+            "dyev-yat    (Девять)",
+            "vo-syem     (Восемь)",
+            "syem        (Семь)",
+            "shest       (Шесть)",
+            "pyat        (Пять)",
+            "chye-tir-ye (Четыре)",
+            "tree        (Три)",
+            "dva         (Два)",
+            "a-deen      (Один)"
+        ]
+    ]
+]
+
 while True:
 
-    try:
-        language = input("Enter '1' for Japanese or '2' for Russian: ")
-    except EOFError:
-        sys.exit(0)
+    while True:
+        try:
+            language = input("Enter '1' for Japanese or '2' for Russian: ")
+        except EOFError:
+            sys.exit(0)
 
-    try:
-        language = int(language)
-    except ValueError:
-        print("Sorry,", language, "is not an integer. Please try again.")
+        try:
+            language = int(language)
+        except ValueError:
+            print("Sorry,", language, "is not an int. Please try again.")
+            print()
+            continue   #Go back up to the word "while"
+
+        if language == 1 or language == 2:
+            break;
+
+        print("Sorry, the int must be 1 or 2. Please try again.")
         print()
-        continue #Go back up to the word "while"
 
-    try:
-        order = input("Enter '1' for ascending order or '2' for descending order: ")
-    except EOFError:
-        sys.exit(0)
+    while True:
+        try:
+            order = input("Enter '1' for ascending order or '2' for descending order: ")
+        except EOFError:
+            sys.exit(0)
 
-    try:
-        order = int(order)
-    except ValueError:
-        print("Sorry,", order, "is not an integer. Please try again.")
+        try:
+            order = int(order)
+        except ValueError:
+            print("Sorry,", order, "is not an int. Please try again.")
+            print()
+            continue   #Go back up to the word "while"
+
+        if order == 1 or order == 2:
+            break;
+
+        print("Sorry, the int must be 1 or 2. Please try again.")
         print()
-        continue #Go back up to the word "while"
 
     print() #Skip a line
 
-    if language == 1 and order == 1:
-        print(1 , " ichi  (いち)")
-        print(2 , " ni    (に)")
-        print(3 , " san   (さん)")
-        print(4 , " yon   (よん)")
-        print(5 , " go    (ご)")
-        print(6 , " roku  (ろく)")
-        print(7 , " nana  (なな)")
-        print(8 , " hachi (はち)")
-        print(9 , " kyuu  (きゅう)")
-        print(10 , "jyuu  (じゅう)")
+    for s in a[language-1][order-1]:
+        print(s)
 
-    elif language == 1 and order == 2:
-        print(10 , "jyuu  (じゅう)")
-        print(9 , " kyuu  (きゅう)")
-        print(8 , " hachi (はち)")
-        print(7 , " nana  (なな)")
-        print(6 , " roku  (ろく)")
-        print(5 , " go    (ご)")
-        print(4 , " yon   (よん)")
-        print(3 , " san   (さん)")
-        print(2 , " ni    (に)")
-        print(1 , " ichi  (いち)")
-
-    elif language == 2 and order == 1:
-        print(1 , " a-deen      (Один)")
-        print(2 , " dva         (Два)")
-        print(3 , " tree        (Три)")
-        print(4 , " chye-tir-ye (Четыре)")
-        print(5 , " pyat        (Пять)")
-        print(6 , " shest       (Шесть)")
-        print(7 , " syem        (Семь)")
-        print(8 , " vo-syem     (Восемь)")
-        print(9 , " dyev-yat    (Девять)")
-        print(10 , "dyes-yat    (Десять)")
-
-    elif language == 2 and order == 2:
-        print(10 , "dyes-yat    (Десять)")
-        print(9 , " dyev-yat    (Девять)")
-        print(8 , " vo-syem     (Восемь)")
-        print(7 , " syem        (Семь)")
-        print(6 , " shest       (Шесть)")
-        print(5 , " pyat        (Пять)")
-        print(4 , " chye-tir-ye (Четыре)")
-        print(3 , " tree        (Три)")
-        print(2 , " dva         (Два)")
-        print(1 , " a-deen      (Один)")
-
-    elif language != 1 and order == 2 \
-    and language != 2 and order == 2:   
-        for i in range(10, 0, -1):
-            print (i)   
-    else:
-        for i in range(1, 11):
-            print(i)
-	
     print() #Skip a line
-
-sys.exit(0)
