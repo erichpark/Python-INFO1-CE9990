c3a9376d11e8e55c364cabc4a7d857090313d40e
Eric Park <ehp349@gmail.com>
2017-07-13 10:04:23 -0400

    Create lyrics.py

---
+++ lyrics.py
@@ -0,0 +1,44 @@
+"""
+lyrics.py
+
+Eric Park
+INFO1-CE9990 Section 2
+Homework for July 6, 2017
+
+Lyrics to the song "12 Days of Christmas"
+
+"""
+
+import sys
+
+lyrics = [
+    "My true love gave to me",
+    "Twelve drummers drumming",
+    "Eleven pipers piping",
+    "Ten lords a-leaping",
+    "Nine ladies dancing",
+    "Eight maids a-milking",
+    "Seven swans a-swimming",
+    "Six geese a-laying",
+    "Five Golden Rings",
+    "Four colly birds",
+    "Three French hens",
+    "Two turtledoves",
+    "And a partridge in a pair tree."
+]
+
+order = [lyrics - 1];
+
+outer = 1
+while outer <= 12:
+
+    inner = 1
+    while inner <= 12:
+        for s in order:
+            print(s)
+        inner +=1
+
+    outer += 1
+
+sys.exit(0)
+
