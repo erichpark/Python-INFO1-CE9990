da3d64206679e2428c12a029e89592b6c4174a6d
Eric Park <ehp349@gmail.com>
2017-07-13 08:56:41 -0400

    Update count.py

--- count.py
+++ count.py
@@ -11,15 +11,13 @@ Count to 10 in Japanese and Russian, ascending and descending
 import sys
 
 #a is a list containing two items: a[0] is Japanese and a[1] is Russian.
-#a[0] is a list containing two items: a[0][0] is Japanese ascending,
-#and a[0][1] is Japanese descending.
-#a[1] is a list containing two items: a[1][0] is Russian ascending,
-#and a[1][1] is Russian descending.
-#Finally, a[0][0] is a list containing 10 items.
+#a[0] is a list containing 10 items.
+#a[1] is a list containing 10 items.
+
 
 a = [
+    
     [
-        [
             "ichi  (いち)", #Japanese ascending, 1 to 10
             "ni    (に)",
             "san   (さん)",
@@ -30,23 +28,10 @@ a = [
             "hachi (はち)",
             "kyuu  (きゅう)",
             "jyuu  (じゅう)"
-        ],
-        [
-            "jyuu  (じゅう)",  #Japanese descending, 10 to 1
-            "kyuu  (きゅう)",
-            "hachi (はち)",
-            "nana  (なな)",
-            "roku  (ろく)",
-            "go    (ご)",
-            "yon   (よん)",
-            "san   (さん)",
-            "ni    (に)",
-            "ichi  (いち)"
-        ],
     ],
+    
 
     [
-        [
             "a-deen      (Один)", #Russian ascending, 1 to 10
             "dva         (Два)",
             "tree        (Три)",
@@ -57,20 +42,8 @@ a = [
             "vo-syem     (Восемь)",
             "dyev-yat    (Девять)",
             "dyes-yat    (Десять)"
-        ],
-        [
-            "dyes-yat    (Десять)", #Russian descending 10 to 1
-            "dyev-yat    (Девять)",
-            "vo-syem     (Восемь)",
-            "syem        (Семь)",
-            "shest       (Шесть)",
-            "pyat        (Пять)",
-            "chye-tir-ye (Четыре)",
-            "tree        (Три)",
-            "dva         (Два)",
-            "a-deen      (Один)"
-        ]
     ]
+
 ]
 
 while True:
@@ -91,8 +64,9 @@ while True:
         if language == 1 or language == 2:
             break;
 
-        print("Sorry, the int must be 1 or 2. Please try again.")
-        print()
+        else:
+            print("Sorry, the int must be 1 or 2. Please try again.")
+            print()
 
     while True:
         try:
@@ -110,12 +84,19 @@ while True:
         if order == 1 or order == 2:
             break;
 
-        print("Sorry, the int must be 1 or 2. Please try again.")
-        print()
+        else:
+            print("Sorry, the int must be 1 or 2. Please try again.")
+            print()
 
     print() #Skip a line
 
-    for s in a[language-1][order-1]:
+
+    numbers = a[language - 1];
+
+    if order == 2:
+        numbers = reversed(numbers)
+
+    for s in numbers:
         print(s)
 
     print() #Skip a line
